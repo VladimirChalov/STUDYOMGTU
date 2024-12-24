6c138e8958ae02fadcf49eedd2ade55e975a1ea8
VladimirChalov <vladimircalov557@gmail.com>
2024-12-24 16:09:46 +0600

    9 лабораторная

---
+++ 9 лабораторная/1 задание.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+
+namespace Omgtu
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+            string g = "";
+            Regex regex = new Regex(@"a.b");
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                g += Convert.ToString(match).Substring(1, 1);
+            }
+
+            char mostFrequentChar = FindMostFrequentChar(g);
+            Console.WriteLine($"Самый часто повторяющийся символ в паттерне: {mostFrequentChar}");
+        }
+
+        static char FindMostFrequentChar(string str)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            char mostFrequentChar = str[0];
+            int maxCount = 0;
+
+            foreach (var kvp in charCount)
+            {
+                if (kvp.Value > maxCount)
+                {
+                    maxCount = kvp.Value;
+                    mostFrequentChar = kvp.Key;
+                }
+            }
+            return mostFrequentChar;
+        }
+    }
+}
