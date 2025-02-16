df9d9f706ab6237271d692ffb6fed28640fdcae0
VladimirChalov <vladimircalov557@gmail.com>
2025-02-16 16:49:20 +0600

    2 семестр АиП

---
+++ АиП 2 семестр/Работа с интерфейсами 2 лабораторная.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Omgtu
+{
+    class Shape
+    {
+        public string Name { get; set; }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+    }
+
+    interface Shape1
+    {
+        double Area();
+        double Perimeter();
+    }
+
+    class Circle : Shape, Shape1
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius) : base("Окружность")
+        {
+            Radius = radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+
+    class Square : Shape, Shape1
+    {
+        public double Side { get; set; }
+
+        public Square(double side) : base("Квадрат")
+        {
+            Side = side;
+        }
+
+        public double Area()
+        {
+            return Side * Side;
+        }
+
+        public double Perimeter()
+        {
+            return 4 * Side;
+        }
+    }
+
+    class Triangle : Shape, Shape1
+    {
+        public double Side { get; set; }
+
+        public Triangle(double side) : base("Равносторонний треугольник")
+        {
+            Side = side;
+        }
+
+        public double Area()
+        {
+            return (Math.Sqrt(3) / 4) * Side * Side;
+        }
+
+        public double Perimeter()
+        {
+            return 3 * Side;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите сторону квадрата: ");
+            double squareSide = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите сторону равностороннего треугольника: ");
+            double triangleSide = Convert.ToDouble(Console.ReadLine());
+
+            Circle circle = new Circle(radius);
+            Square square = new Square(squareSide);
+            Triangle triangle = new Triangle(triangleSide);
+
+            Console.WriteLine("Окружность");
+            Console.WriteLine($"Площадь: {circle.Area()}");
+            Console.WriteLine($"Периметр: {circle.Perimeter()}");
+
+            Console.WriteLine("Квадрат");
+            Console.WriteLine($"Площадь: {square.Area()}");
+            Console.WriteLine($"Периметр: {square.Perimeter()}");
+
+            Console.WriteLine("Равносторонний треугольник");
+            Console.WriteLine($"Площадь: {triangle.Area()}");
+            Console.WriteLine($"Периметр: {triangle.Perimeter()}");
+        }
+    }
+}
+
---
+++ АиП 2 семестр/Телефоны 1 лабораторная.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace Omgtu
+{
+    class Phone
+    {
+        public string Surname { get; set; }
+        public int[] Numbers { get; set; }
+        public string[] Operators { get; set; }
+        public int Year { get; set; }
+
+        
+
+        public Phone(string surname, int[] numbers, string[] operators, int year)
+        {
+            Surname = surname;
+            Numbers = numbers;
+            Operators = operators;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            string numbers = string.Join(", ", Numbers);
+            string operators = string.Join(", ", Operators);
+            return $"Фамилия: {Surname}, Номера телефонов: {numbers}, Операторы: {operators}, Год подключения: {Year}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество пользователей:");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            Phone[] phones = new Phone[count];
+            int index = 0;
+            Console.WriteLine("Введите количество номеров телефона для пользователей:");
+            int MaxNumbers = Convert.ToInt32(Console.ReadLine()); 
+
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Внести пользователя");
+                Console.WriteLine("2. Сортировка по фамилии пользователя");
+                Console.WriteLine("3. Сортировка по номеру телефона");
+                Console.WriteLine("4. Сортировка по сотовому оператору");
+                Console.WriteLine("5. Сортировка по году подключения");
+                Console.WriteLine("6. Выход");
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                switch (n)
+                {
+                    case 1:
+                        if (index < count)
+                        {
+                            Console.WriteLine("Введите фамилию:");
+                            string surname = Console.ReadLine();
+                            Console.WriteLine("Введите год подключения:");
+                            int year = Convert.ToInt32(Console.ReadLine());
+                            int[] numbers = new int[MaxNumbers];
+                            string[] operators = new string[MaxNumbers];
+
+
+
+                            for (int i = 0; i < MaxNumbers; i++)
+                            {
+                                Console.WriteLine($"Введите номер телефона {i + 1}:");
+                                string input = Console.ReadLine();
+
+                                numbers[i] = Convert.ToInt32(input);
+
+                                Console.WriteLine("Введите оператор сотовой связи:");
+                                operators[i] = Console.ReadLine();
+                            }
+
+
+                            phones[index] = new Phone(surname, numbers, operators, year);
+                            index++;
+                        }
+                        break;
+
+                    case 2:
+                        if (index > 0)
+                        {
+                            Console.WriteLine("Введите фамилию для выборки:");
+                            string surnameToFilter = Console.ReadLine();
+
+                            for (int i = 0; i < index; i++)
+                            {
+                                if (phones[i].Surname==surnameToFilter)
+                                {
+                                    Console.WriteLine(phones[i]);
+                                }
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        if (index > 0)
+                        {
+                            Console.WriteLine("Введите номер для выборки:");
+                            int numberToFilter = Convert.ToInt32(Console.ReadLine());
+
+                            for (int i = 0; i < index; i++)
+                            {
+                                for (int j = 0; j < MaxNumbers; j++)
+                                {
+                                    if (phones[i].Numbers[j] == numberToFilter)
+                                    {
+                                        Console.WriteLine(phones[i]);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        if (index > 0)
+                        {
+                            Console.WriteLine("Введите оператор для выборки:");
+                            string operToFilter = Console.ReadLine();
+
+                            for (int i = 0; i < index; i++)
+                            {
+                                for (int j = 0; j < MaxNumbers; j++)
+                                {
+                                    if (phones[i].Operators[j]==operToFilter )
+                                    {
+                                        Console.WriteLine(phones[i]);
+                                    }
+                                }
+                            }
+
+                        }
+                        break;
+
+                    case 5:
+                        if (index > 0)
+                        {
+                            Console.WriteLine("Введите год для выборки:");
+                            int yearToFilter = Convert.ToInt32(Console.ReadLine());
+
+                            for (int i = 0; i < index; i++)
+                            {
+                                if (phones[i].Year == yearToFilter)
+                                {
+                                    Console.WriteLine(phones[i]);
+                                }
+                            }
+                        }
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Выход из программы");
+                        return;
+
+
+                }
+            }
+        }
+    }
+}
+
