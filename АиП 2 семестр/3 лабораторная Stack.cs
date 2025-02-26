8c4ba3b7c4e8b682d6672237b3d3014651e05959
VladimirChalov <vladimircalov557@gmail.com>
2025-02-26 19:58:30 +0600

    3 лабораторная на Stack

---
+++ АиП 2 семестр/3 лабораторная Stack.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace Omgtu
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите последовательность скобок: ");
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in input)
+            {
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+                    stack.Push(ch);
+                }
+                else if (ch == ')' || ch == ']' || ch == '}')
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("Скобки расставлены неправильно.");
+                        break;
+                    }
+
+                    char open = stack.Pop();
+                    if ((open == '(' && ch != ')') ||
+                        (open == '[' && ch != ']') ||
+                        (open == '{' && ch != '}'))
+                    {
+                        Console.WriteLine("Скобки расставлены неправильно.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Скобки расставлены правильно.");
+                    }
+                }
+            }
+        }
+    }
+}
+
