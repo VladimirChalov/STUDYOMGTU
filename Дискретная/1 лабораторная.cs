0a62327a93b08f83db320571523c8713466eaf55
VladimirChalov <vladimircalov557@gmail.com>
2025-03-01 11:15:52 +0600

    Лабораторная работы по дискретной математике

---
+++ Дискретная/1 лабораторная.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Omgtu
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] praph = {
+            {0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1},
+            {0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 1, 1, 0}
+        };
+
+            List<int> verifiedNum = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> numDone = new List<int> { 0 };
+            int result = 0;
+            int shag = 0;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                int m = 0;
+                int n;
+
+                if (shag > (numDone.Count - 1))
+                {
+                    n = verifiedNum[0];
+                    numDone.Add(n);
+                    res++;
+                }
+                else
+                {
+                    n = numDone[shag];
+                }
+
+                for (m = 0; m < 6; m++)
+                {
+                    if (praph[n, m] == 1)
+                    {
+                        if (verifiedNum.Contains(m))
+                        {
+                            verifiedNum.Remove(m);
+                        }
+                        if (!numDone.Contains(m))
+                        {
+                            numDone.Add(m);
+                        }
+                    }
+                }
+                shag++;
+            }
+
+            Console.WriteLine(result + 1);
+        }
+    }
+}
---
+++ Дискретная/3 лабораторная.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Omgtu
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] graph = new int[,]
+            {
+            { 0, 400, 0, 0, 511 },
+            { 600, 0, 155, 0, 200 },
+            { 0, 101, 0, 409, 0 },
+            { 0, 0, 444, 0, 800 },
+            { 505, 212, 0, 334, 0 }
+            };
+
+            Console.Write("Введите начальную вершину (0-4): ");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите конечную вершину (0-4): ");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+            Dijkstra(graph, start, end);
+        }
+
+        static void Dijkstra(int[,] graph, int startVertex, int endVertex)
+        {
+            int V = graph.GetLength(0);
+            int[] distances = new int[V];
+            bool[] visited = new bool[V];
+
+            for (int i = 0; i < V; i++)
+            {
+                distances[i] = int.MaxValue;
+                visited[i] = false;
+            }
+
+            distances[startVertex] = 0;
+
+            for (int count = 0; count < V - 1; count++)
+            {
+                int u = FindMinDistanceVertex(distances, visited, V);
+                visited[u] = true;
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (graph[u, v] != 0 && !visited[v] && distances[u] != int.MaxValue && distances[u] + graph[u, v] < distances[v])
+                    {
+                        distances[v] = distances[u] + graph[u, v];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Минимальное расстояние от вершины {startVertex} до вершины {endVertex}: {distances[endVertex]}");
+        }
+
+        static int FindMinDistanceVertex(int[] distances, bool[] visited, int V)
+        {
+            int minValue = int.MaxValue;
+            int minIndex = -1;
+
+            for (int v = 0; v < V; v++)
+            {
+                if (!visited[v] && distances[v] <= minValue)
+                {
+                    minValue = distances[v];
+                    minIndex = v;
+                }
+            }
+            return minIndex;
+        }
+    }
+}
+
