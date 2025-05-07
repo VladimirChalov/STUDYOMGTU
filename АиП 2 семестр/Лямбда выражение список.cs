099bba24adc0e496e4fa58b5b1b886ad245f4167
VladimirChalov <vladimircalov557@gmail.com>
2025-05-07 22:36:54 +0600

    Add files via upload

---
+++ АиП 2 семестр/Лямбда выражение список.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        List<string> slova = new List<string> { "панда", "яблоко", "птица", "машина", "пельмени", "дом", "планета" };
+
+        List<string> otobrannieSlova = slova.Where(slovo => slovo.StartsWith("п")).ToList();
+
+        Console.WriteLine("Слова, начинающиеся на букву 'п':");
+        foreach (var slovo in otobrannieSlova)
+        {
+            Console.WriteLine(slovo);
+        }
+    }
+}
